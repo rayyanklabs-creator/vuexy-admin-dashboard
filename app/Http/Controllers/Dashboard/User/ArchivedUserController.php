8fc537f45523d3068f624175a4f46629fd042326
Rayyan <rayyan@byklabs.com>
2025-10-25 01:54:40 +0500

    fixes and optimize the user and archived user controller

--- app/Http/Controllers/Dashboard/User/ArchivedUserController.php
+++ app/Http/Controllers/Dashboard/User/ArchivedUserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Dashboard\User;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use App\Services\UserService;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
@@ -12,14 +13,22 @@ class ArchivedUserController extends Controller
 {
     use AuthorizesRequests;
 
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         $this->authorize('view archived user');
+
         try {
-            $archivedUsers = User::onlyTrashed()->get();
+            $archivedUsers = $this->userService->getArchivedUsersQuery()->get();
             return view('dashboard.users.archived.index', compact('archivedUsers'));
         } catch (\Throwable $th) {
             Log::error("Archived User Index Failed:" . $th->getMessage());
@@ -33,10 +42,11 @@ class ArchivedUserController extends Controller
     public function destroy(string $id)
     {
         $this->authorize('delete archived user');
-        try {
-            $user = User::withTrashed()->findOrFail($id);
 
+        try {
+            $user = $this->userService->getArchivedUserById($id);
             $user->forceDelete();
+
             return redirect()->route('dashboard.archived-user.index')->with('success', 'User Permanently Deleted Successfully');
         } catch (\Throwable $th) {
             Log::error("Archived User destroy Failed:" . $th->getMessage());
@@ -44,16 +54,16 @@ class ArchivedUserController extends Controller
         }
     }
 
-    public function restoreUser($id)
+     public function restoreUser($id)
     {
         $this->authorize('update archived user');
+        
         try {
-            $user = User::onlyTrashed()->findOrFail($id);
+            $user = $this->userService->getArchivedUserById($id);
             $user->restore();
+            
             return redirect()->route('dashboard.archived-user.index')->with('success', 'User Restored Successfully');
         } catch (\Throwable $th) {
-            // Handle the exception
-            // throw $th;
             Log::error("Archived User restore Failed:" . $th->getMessage());
             return redirect()->back()->with('error', "Something went wrong! Please try again later");
         }
--- app/Http/Controllers/Dashboard/User/UserController.php
+++ app/Http/Controllers/Dashboard/User/UserController.php
@@ -5,50 +5,51 @@ namespace App\Http\Controllers\Dashboard\User;
 use App\Http\Controllers\Controller;
 use App\Models\Profile;
 use App\Models\User;
-use App\trait\GenerateUsername;
+use App\Services\UserService;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rules\Password;
-use Spatie\Permission\Models\Role;
 
 class UserController extends Controller
 {
-    use AuthorizesRequests, GenerateUsername;
+    use AuthorizesRequests;
+    protected $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
 
     public function index()
     {
-
         $this->authorize('view user');
+
         try {
-            $users = User::with('profile')->get();
-            $totalUsers = User::count();
-            $totalDeactivatedUsers = User::where('is_active', 'inactive')->count();
-            $totalActiveUsers = User::where('is_active', 'active')->count();
-            $totalUnverifiedUsers = User::where('email_verified_at', null)->count();
-            $totalArchivedUsers = User::onlyTrashed()->count();
-            $roles = Role::all();
-            return view('dashboard.users.index', compact('users', 'totalUsers', 'totalDeactivatedUsers', 'totalActiveUsers', 'totalUnverifiedUsers', 'roles', 'totalArchivedUsers'));
+            $users = $this->userService->getUsersWithProfile()->get();
+            $stats = $this->userService->getUserStats();
+            $roles = $this->userService->getRoles();
+
+            return view('dashboard.users.index', compact('users', 'roles') + $stats);
         } catch (\Throwable $th) {
             Log::error("User Index Failed:" . $th->getMessage());
             return redirect()->back()->with('error', "Something went wrong! Please try again later");
         }
     }
 
-
-    /**
-     * Store a newly created resource in storage.
-     */
+    // /**
+    //  * Store a newly created resource in storage.
+    //  */
     public function store(Request $request)
     {
         $this->authorize('create user');
+
         $validate = Validator::make($request->all(), [
             'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
+            'last_name' => 'nullable|string|max:255',
             'email' => 'required|email|max:255|unique:users,email',
             'password' => [
                 'required',
@@ -69,23 +70,15 @@ class UserController extends Controller
 
         try {
             DB::transaction(function () use ($request) {
-
-                $user = new User();
-                $user->name = $request->first_name . ' ' . $request->last_name;
-                $user->email = $request->email;
-                $user->password = Hash::make($request->password);
-                $user->email_verified_at = now();
-                $user->username = $this->generateUsername($request->first_name . ' ' . $request->last_name);
-                $user->save();
+                $userData = $this->userService->generateUserData($request->all());
+                $user = User::create($userData);
 
                 $user->syncRoles($request->role);
 
-                $profile = new Profile();
-                $profile->user_id = $user->id;
-                $profile->first_name = $request->first_name;
-                $profile->last_name = $request->last_name;
-                $profile->save();
+                $profileData = $this->userService->generateProfileData($user->id, $request->all());
+                Profile::create($profileData);
             });
+
             return redirect()->route('dashboard.user.index')->with('success', 'User created successfully');
         } catch (\Throwable $th) {
             DB::rollback();
@@ -103,31 +96,14 @@ class UserController extends Controller
         if (!Gate::any(['update user', 'view user'])) {
             abort(403, 'Unauthorized');
         }
+
         try {
-            $user = User::with('profile')->findOrFail($id);
+            $user = $this->userService->getUserById($id);
             return response()->json([
                 'success' => true,
-                'user' => [
-                    'id' => $user->id,
-                    'first_name' => $user->profile->first_name,
-                    'last_name' => $user->profile->last_name ?? null,
-                    'email' => $user->email ?? null,
-                    'username' => $user->username ?? null,
-                    'role' => $user->getRoleNames()->first(),
-                    'full_name' => $user->name,
-                    'is_active' => $user->is_active,
-                    'profile_image' => $user->profile->profile_image ?? null,
-                    'dob' => $user->profile->dob ?? null,
-                    'phone_number' => $user->profile->phone_number,
-                    'bio' => $user->profile->bio ?? null,
-                    'facebook_url' => $user->profile->facebook_url ?? null,
-                    'linkedin_url' => $user->profile->linkedin_url ?? null,
-                    'instagram_url' => $user->profile->instagram_url ?? null,
-                    'github_url' => $user->profile->github_url ?? null,
-                ],
+                'user' => $this->userService->formatUserData($user),
             ]);
         } catch (\Throwable $th) {
-            // throw $th;
             Log::error("User Edit Failed:" . $th->getMessage());
             return response()->json([
                 'success' => false,
@@ -137,12 +113,14 @@ class UserController extends Controller
     }
 
 
+
     /**
      * Update the specified resource in storage.
      */
     public function update(Request $request, string $id)
     {
         $this->authorize('update user');
+
         $validate = Validator::make($request->all(), [
             'edit_first_name' => 'required|string|max:255',
             'edit_last_name' => 'required|string|max:255',
@@ -154,20 +132,18 @@ class UserController extends Controller
         }
 
         try {
-            DB::beginTransaction();
+            DB::transaction(function () use ($id, $request) {
+                $user = $this->userService->getUserById($id, false);
+                $user->name = $request->edit_first_name . ' ' . $request->edit_last_name;
+                $user->save();
 
-            $user = User::findOrFail($id);
-            $user->name = $request->edit_first_name . ' ' . $request->edit_last_name;
-            $user->save();
+                $user->syncRoles($request->edit_role);
 
-            $user->syncRoles($request->edit_role);
-
-            $profile = Profile::where('user_id', $user->id)->firstOrFail();
-            $profile->first_name = $request->edit_first_name;
-            $profile->last_name = $request->edit_last_name;
-            $profile->save();
-
-            DB::commit();
+                $profile = Profile::where('user_id', $user->id)->firstOrFail();
+                $profile->first_name = $request->edit_first_name;
+                $profile->last_name = $request->edit_last_name;
+                $profile->save();
+            });
 
             return redirect()->route('dashboard.user.index')->with('success', 'User updated successfully');
         } catch (\Throwable $th) {
@@ -184,14 +160,15 @@ class UserController extends Controller
     public function destroy(string $id)
     {
         $this->authorize('delete user');
+
         try {
-            $user = User::findOrFail($id);
+            $user = $this->userService->getUserById($id, false);
             $user->delete();
+
             return redirect()->back()->with('success', 'Account Deleted Successfully');
         } catch (\Throwable $th) {
             Log::error('Account Deletion Failed', ['error' => $th->getMessage()]);
             return redirect()->back()->with('error', "Something went wrong! Please try again later");
-            throw $th;
         }
     }
 
@@ -203,21 +180,18 @@ class UserController extends Controller
     public function updateStatus(string $id)
     {
         $this->authorize('update user');
+
         try {
-            $user = User::findOrFail($id);
+            $user = $this->userService->getUserById($id, false);
             $message = $user->is_active == 'active' ? 'Account Deactivated Successfully' : 'Account Activated Successfully';
-            if ($user->is_active == 'active') {
-                $user->is_active = 'inactive';
-                $user->saveQuietly();
-            } else {
-                $user->is_active = 'active';
-                $user->saveQuietly();
-            }
+
+            $user->is_active = $user->is_active == 'active' ? 'inactive' : 'active';
+            $user->saveQuietly();
+
             return redirect()->back()->with('success', $message);
         } catch (\Throwable $th) {
             Log::error('Account Status Updation Failed', ['error' => $th->getMessage()]);
             return redirect()->back()->with('error', "Something went wrong! Please try again later");
-            throw $th;
         }
     }
 }
